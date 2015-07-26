16e7486ec7ef34a1a410f7cfdc092c5ba25e150c
ycl <y20080328@126.com>
2015-07-26 23:17:43 +0800

    merge disunitycli

--- disunity-cli/src/main/java/info/ata4/disunity/cli/DisUnityCli.java
+++ disunity-cli/src/main/java/info/ata4/disunity/cli/DisUnityCli.java
@@ -25,24 +25,17 @@ import info.ata4.disunity.cli.command.LearnCommand;
 import info.ata4.disunity.cli.command.ListCommand;
 import info.ata4.log.LogUtils;
 import info.ata4.unity.DisUnity;
-<<<<<<< HEAD
 
-import java.io.File;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-=======
-import java.io.PrintWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
->>>>>>> 623edb4c109259c6ef4e79c4de3e04bd4b1242f2
+
 import org.apache.commons.io.output.CloseShieldOutputStream;
 
 /**
  * DisUnity command line interface.
-<<<<<<< HEAD
+ * <<<<<<< HEAD
  *
  * @author Nico Bergemann <barracuda415 at yahoo.de>
  */
@@ -54,19 +47,6 @@ public class DisUnityCli implements Runnable {
     private final DisUnityOptions opts = new DisUnityOptions();
     private final JCommander jc = new JCommander();
 
-=======
- * 
- * @author Nico Bergemann <barracuda415 at yahoo.de>
- */
-public class DisUnityCli implements Runnable {
-    
-    private static final Logger L = LogUtils.getLogger();
-    private static final boolean DEBUG = true;
-    
-    private final DisUnityOptions opts = new DisUnityOptions();
-    private final JCommander jc = new JCommander();
-    
->>>>>>> 623edb4c109259c6ef4e79c4de3e04bd4b1242f2
     public DisUnityCli() {
         jc.setProgramName(DisUnity.getProgramName());
         jc.addObject(opts);
@@ -83,21 +63,11 @@ public class DisUnityCli implements Runnable {
         jc.addCommand(new LearnCommand());
         jc.addCommand(new ListCommand());
 //        jc.addCommand(new SplitCmd());
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> 623edb4c109259c6ef4e79c4de3e04bd4b1242f2
         // bundle commands
         jc.addCommand(new BundleExtractCommand());
         jc.addCommand(new BundleBuildCommand());
         jc.addCommand(new BundleListCommand());
         jc.addCommand(new BundleInfoCommand());
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> 623edb4c109259c6ef4e79c4de3e04bd4b1242f2
         // debug commands
         if (DEBUG) {
             jc.addCommand(new DebugAssetTest());
@@ -105,45 +75,23 @@ public class DisUnityCli implements Runnable {
             jc.addCommand(new DebugBundleCopy());
         }
     }
-<<<<<<< HEAD
 
     public void parse(String[] args) {
         jc.parse(args);
 
-=======
-    
-    public void parse(String[] args) {
-        jc.parse(args);
-        
->>>>>>> 623edb4c109259c6ef4e79c4de3e04bd4b1242f2
         // display usage
         if (opts.isHelp()) {
             jc.usage();
         }
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> 623edb4c109259c6ef4e79c4de3e04bd4b1242f2
         // increase logging level if requested
         if (opts.isVerbose()) {
             LogUtils.configure(Level.ALL);
         }
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> 623edb4c109259c6ef4e79c4de3e04bd4b1242f2
         // only print warnings and errors to stderr if the stdout format is not
         // plain text
         if (opts.getOutputFormat() != OutputFormat.PLAINTEXT) {
             LogUtils.configure(Level.WARNING);
         }
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> 623edb4c109259c6ef4e79c4de3e04bd4b1242f2
         L.info(DisUnity.getSignature());
     }
 
@@ -152,25 +100,14 @@ public class DisUnityCli implements Runnable {
         if (opts.isHelp()) {
             return;
         }
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> 623edb4c109259c6ef4e79c4de3e04bd4b1242f2
         String cmdName = jc.getParsedCommand();
         if (cmdName == null) {
             jc.usage();
             return;
         }
-<<<<<<< HEAD
 
         JCommander jcc = jc.getCommands().get(cmdName);
 
-=======
-        
-        JCommander jcc = jc.getCommands().get(cmdName);
-        
->>>>>>> 623edb4c109259c6ef4e79c4de3e04bd4b1242f2
         try (PrintWriter out = new PrintWriter(new CloseShieldOutputStream(System.out), true)) {
             Command cmd = (Command) jcc.getObjects().get(0);
             cmd.setOutputWriter(out);
@@ -178,27 +115,17 @@ public class DisUnityCli implements Runnable {
             cmd.run();
         }
     }
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> 623edb4c109259c6ef4e79c4de3e04bd4b1242f2
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         LogUtils.configure();
-<<<<<<< HEAD
 
         DisUnityCli cli = new DisUnityCli();
         args = new String[2];
         args[0] = "extract";
         args[1] = "/Users/shuuseiyang/Documents/workspace/unity3d/efcc/";
-=======
-        
-        DisUnityCli cli = new DisUnityCli();
-        
->>>>>>> 623edb4c109259c6ef4e79c4de3e04bd4b1242f2
         try {
             cli.parse(args);
             cli.run();
@@ -207,7 +134,6 @@ public class DisUnityCli implements Runnable {
         } catch (Throwable t) {
             L.log(Level.SEVERE, "Fatal error", t);
         }
-<<<<<<< HEAD
 //        File file = new File(args[1]);
 //        if (file.isDirectory()) {
 //            for (String filename : file.list()) {
@@ -234,7 +160,4 @@ public class DisUnityCli implements Runnable {
 //        }
     }
 
-=======
-    }
->>>>>>> 623edb4c109259c6ef4e79c4de3e04bd4b1242f2
 }
